31cb831b682d56565faa2fb61c1fef1f8fe6bf38
ma-za-kpe <makpalyy@gmail.com>
2018-07-02 03:32:57 +0300

    check if the value entered is a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 //business logic
 function pingpong(number) {
 
-  
+  if (typeof number === "number") {
+    
+  } else {
+    return "enter a number"
+  }
+
+/**var one = 1;
+for (var i = one; i <= number; i++) {
+
+  if (i % 15 === 0 ) {
+    console.log("pingpong");
+  }else if (i % 3 === 0) {
+    console.log("ping");
+  }else if (i % 5 === 0) {
+    console.log("pong");
+  }else {
+    console.log(i);
+  }
+
+}*/
 
 }
 
