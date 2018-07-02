4796f777f7e7c24af248ac41183b16ee45ce66c5
ma-za-kpe <makpalyy@gmail.com>
2018-07-02 03:27:25 +0300

    remove footer tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 //business logic
+function pingpong(number) {
+
+  var one = 1;
+  for (var i = one; i <= number; i++) {
+
+    if (i % 15 === 0 ) {
+      console.log("pingpong");
+    }else if (i % 3 === 0) {
+      console.log("ping");
+    }else if (i % 5 === 0) {
+      console.log("pong");
+    }else {
+      console.log(i);
+    }
+
+  }
+
+}
 
 
 //UI logic
